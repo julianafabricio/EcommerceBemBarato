7e0b393947b12777a2df435f311d5a6dae891878
Juliana Fabricio <julianafabriciosoares24@gmail.com>
2024-08-10 13:53:56 -0300

    Criado html das páginas index e user

--- bembarato.py
+++ bembarato.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, make_response, request
+from markupsafe import escape
+from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p> BEM BARATO.COM </p>"
+    return render_template('index.html')
 
 @app.route("/sobre")
 def sobre():
@@ -14,3 +17,17 @@ def sobre():
 @app.route("/sobre/privacidade")
 def privacidade():
     return "<h4>No Bem Barato.com, a sua privacidade é nossa prioridade. </h4>"
+
+@app.route("/user/<username>")
+def username(username):
+    cok = make_response("<h2>cookie criado</h2>")
+    cok.set_cookie('username', username)
+    return cok
+
+@app.route("/user2/")
+@app.route("/user2/<username>")
+def username2(username=None):
+    cokusername = request.cookies.get('username')
+    print(cokusername)
+    return render_template('user.html',username=username, cokusername=cokusername)
+
